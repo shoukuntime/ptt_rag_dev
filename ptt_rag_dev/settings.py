2ef4f8e7b4567be1a4bb8c782fe015e43882e5d9
shouchen <jenjiunchen@eland.com.tw>
2025-08-01 11:12:19 +0800

    【Day 03】環境建置與必要工具安裝 - MariaDB

--- ptt_rag_dev/settings.py
+++ ptt_rag_dev/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'ptt_rag_dev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
