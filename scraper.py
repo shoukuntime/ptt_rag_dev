e1fd4535ab57ff2dcd0c4e12c55b1d187d4d446d
shouchen <jenjiunchen@eland.com.tw>
2025-08-06 14:49:15 +0800

    【Day 07】建立基本爬蟲抓取文章列表與內容 – 寫出完整爬蟲程式

--- scraper.py
+++ scraper.py
@@ -1,5 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+def get_html(url: str) -> str:
+    session = requests.Session()
+    payload = {
+        "from": url,
+        "yes": "yes"
+    }
+    session.post("https://www.ptt.cc/ask/over18", data=payload)
+    response = session.get(url)
+    return response.text
+
+
+def get_urls_from_board_html(html: str) -> list:
+    html_soup = BeautifulSoup(html, 'html.parser')
+    r_ent_all = html_soup.find_all('div', class_='r-ent')
+    urls = []
+    for r_ent in r_ent_all:
+        # 若無r_ent.find('a')['href']代表文章已刪除
+        if r_ent.find('a'):
+            if r_ent.find('a')['href']:
+                urls.append('https://www.ptt.cc' + r_ent.find('a')['href'])
+    return urls
+
+
+def get_data_from_article_html(html: str) -> dict:
+    html_soup = BeautifulSoup(html, 'html.parser')
+    article_soup = html_soup.find('div', class_='bbs-screen bbs-content')
+    title = article_soup.find_all('span', class_='article-meta-value')[2].text
+    author = article_soup.find_all('span', class_='article-meta-value')[0].text.strip(')').split(' (')[0]
+    time_str = article_soup.find_all('span', class_='article-meta-value')[3].text
+    dt = datetime.strptime(time_str, "%a %b %d %H:%M:%S %Y")
+    dt = dt.replace(tzinfo=ZoneInfo("Asia/Taipei"))
+    post_time = dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    result = []
+    for element in article_soup.children:
+        if element.name not in ["div", "span"]:
+            text = element.get_text(strip=True) if element.name == "a" else str(element).strip()
+            if text:
+                result.append(text)
+    content = "\n".join(result).strip('-')
+
+    data = {
+        'title': title,
+        'author': author,
+        'post_time': post_time,
+        'content': content,
+    }
+    return data
 
 
 def ptt_scrape(board: str) -> list:
@@ -13,18 +65,6 @@ def ptt_scrape(board: str) -> list:
         article_data.update({'board': board})  # 加入版面名稱資訊
         article_datas.append(article_data)  # 將文章資訊蒐集起來
     return article_datas  # 回傳文章資訊列表
-
-
-def get_html(url: str) -> str:
-    ...
-
-
-def get_urls_from_board_html(html: str) -> list:
-    ...
-
-
-def get_data_from_article_html(html: str) -> dict:
-    ...
 
 
 if __name__ == "__main__":
