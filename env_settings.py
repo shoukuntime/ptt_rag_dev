5cc6a98f54f7e5ee592751897bb8b65daefa2115
shouchen <jenjiunchen@eland.com.tw>
2025-08-14 11:04:55 +0800

    【Day 17】建立向量資料庫基礎 - Gemini Embedding 與 Pinecone 初步串接

---
+++ env_settings.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent
+
+class EnvSettings(BaseSettings):
+    GOOGLE_API_KEY : str = None
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / '.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
